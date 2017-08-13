844deade1fe9c0b0be2da173eea6f43860f2fd70
Matthew Dowse <matthewdowse@learniosfreecoms-mac.local>
2017-08-13 10:53:42 +0100

    Initial Commit

---
+++ PomoTime WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PomoTime WatchKit Extension
+//
+//  Created by Matthew Dowse on 13/08/2017.
+//  Copyright © 2017 Matthew Dowse. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
