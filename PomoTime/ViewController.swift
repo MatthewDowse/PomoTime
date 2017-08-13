584cab62074e04122baa50e22d9385204e4c0742
Matthew Dowse <matthewdowse@learniosfreecoms-mac.local>
2017-08-13 11:11:21 +0100

    Added a simple change to the view controller

--- PomoTime/ViewController.swift
+++ PomoTime/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("THis will be my first change commited to Git")
     }
 
     override func didReceiveMemoryWarning() {
